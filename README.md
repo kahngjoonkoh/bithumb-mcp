76c74c1ea7dfe7b1e29091e1010d4cdb638717a8
Kahngjoon Koh <kahngjoonk@gmail.com>
2025-04-08 21:31:00 +0900

    feat - added more tools. Added a requirement for tabulated printing

--- README.md
+++ README.md
@@ -4,8 +4,7 @@
 
 This is a Model Context Protocol (MCP) server for Bithumb, allowing LLMs like Claude to interact with the Bithumb trading API. It enables trading stocks, checking positions, fetching market data, and managing your account - all through natural language.
 
-It relies on Bithumb API 2.1 which is currently in beta version.
-up to date for API 2.1
+It relies on Bithumb API v2.1.0. Will soon to have support for v2.1.5 which is currently in beta version.
 
 ## Prerequisites
 
@@ -26,3 +25,18 @@ fastmcp install server.py \
     -e API_KEY_ID=<YOUR_API_KEY> \
     -e API_SECRET_KEY=<YOUR_API_SECRET_KEY>
 ```
+This should automatically configure your `claude_desktop_config.json` file.
+
+## Example Queries
+* What's my current account balance?
+* Get me a snapshot for SUI
+* Show me the price history for SUI over the last 100 days
+* Buy 10000 KRW worth of SUI at a limit price of 2600 KRW
+* Analyse the orderbook and give me a rough estimate of the chances for the price of SUI going up or down. 
+* Cancel all my open orders.
+
+## !! NOTICE !!
+This MCP server will have access to real trades. The developers of this server will not be held accountable for mistakes or bad decision making.
+
+## License
+This project is licensed under the [MIT License](https://github.com/kahngjoonkoh/pybithumb2/blob/main/LICENSE). See the [LICENSE](https://github.com/kahngjoonkoh/pybithumb2/blob/main/LICENSE) file for more details.
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import os
 from typing import Optional, List
 from decimal import Decimal
+from tabulate import tabulate
 from dotenv import load_dotenv
 
 from mcp.server.fastmcp import FastMCP
-from pybithumb2 import BithumbClient, MarketID, TradeSide, OrderType, OrderID
+from pybithumb2 import BithumbClient, MarketID, TradeSide, OrderType, OrderID, TimeUnit
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY_ID")
@@ -17,6 +18,10 @@ if not API_KEY or not API_SECRET:
 mcp = FastMCP("Bithumb API", dependencies=["pybithumb2", "python-dotenv"])
 
 client = BithumbClient(API_KEY, API_SECRET, use_raw_data=True)
+
+
+def _format_helper(data: dict) -> str:
+    return tabulate(data, headers="keys", tablefmt="plain")
 
 
 @mcp.tool()
@@ -82,13 +87,160 @@ async def get_markets() -> dict:
     ]
 
 
-# @mcp.tool()
-# async def get_minute_candles(
-#     market: str,
-#     to: Optional[str] = None
-#     count: int = 1
-#     unit:
-# )
+@mcp.tool()
+async def get_minute_candles(
+    market: str, to: Optional[str] = None, count: int = 1, unit: int = 1
+) -> str:
+    """
+    Fetches minute candlesticks for a given market.
+
+    Args:
+        market (str): The market to fetch minute candles for (e.g., "KRW-BTC").
+        to (str, optional): A datetime with the format of (%Y-%m-%d %H:%M:%S) to limit the candlesticks to. Defaults to None.
+        count (int, optional): The number of candles to fetch. Defaults to 1.
+        unit (int, optional): The candle unit in minutes. Can only take values of 1, 3, 5, 10, 15, 30, 60, 240. Defaults to 1 minute.
+
+    Returns:
+        str: The response containing the minute candles data as a formatted table.
+    """
+    data = client.get_minute_candles(
+        market=MarketID.from_string(str), to=to, count=count, unit=TimeUnit(unit)
+    )
+    # using tabulate instead of pandas as its more lightweight.
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_day_candles(
+    market: str,
+    to: Optional[str] = None,
+    count: int = 1,
+    unit: Optional[int] = 1,
+    convertingPriceUnit: str = None,
+) -> str:
+    """
+    Fetches minute candlesticks for a given market.
+
+    Args:
+        market (str): The market to fetch minute candles for (e.g., "KRW-BTC").
+        to (str, optional): A datetime with the format of (%Y-%m-%d %H:%M:%S) to limit the candlesticks to. Defaults to None.
+        count (int, optional): The number of candles to fetch. Defaults to 1.
+        convertingPriceUnit (str, optional): The converting price unit as a symbol. (e.g. KRW, BTC)
+
+    Returns:
+        str: The response containing the day candles data as a formatted table.
+    """
+    data = client.get_day_candles(
+        market=MarketID.from_string(str),
+        to=to,
+        count=count,
+        convertingPriceUnit=convertingPriceUnit,
+    )
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_week_candles(
+    market: str,
+    to: Optional[str] = None,
+    count: int = 1,
+) -> str:
+    """
+    Fetches minute candlesticks for a given market.
+
+    Args:
+        market (str): The market to fetch week candles for (e.g., "KRW-BTC").
+        to (str, optional): A datetime with the format of (%Y-%m-%d %H:%M:%S) to limit the candlesticks to. Defaults to None.
+        count (int, optional): The number of candles to fetch. Defaults to 1.
+
+    Returns:
+        str: The response containing the week candles data as a formatted table.
+    """
+    data = client.get_week_candles(market=MarketID.from_string(str), to=to, count=count)
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_month_candles(
+    market: str,
+    to: Optional[str] = None,
+    count: int = 1,
+) -> str:
+    """
+    Fetches minute candlesticks for a given market.
+
+    Args:
+        market (str): The market to fetch month candles for (e.g., "KRW-BTC").
+        to (str, optional): A datetime with the format of (%Y-%m-%d %H:%M:%S) to limit the candlesticks to. Defaults to None.
+        count (int, optional): The number of candles to fetch. Defaults to 1.
+
+    Returns:
+        str: The response containing the month candles data as a formatted table.
+    """
+    data = client.get_month_candles(
+        market=MarketID.from_string(str), to=to, count=count
+    )
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_trades(
+    market: str, to: Optional[str] = None, count: int = 1, daysAgo: Optional[int] = None
+) -> str:
+    """
+    Gets trade prices.
+
+    Args:
+        market (str): The market to fetch trade date for (e.g., "KRW-BTC").
+        to (str, optional): A datetime with the format of (%Y-%m-%d %H:%M:%S) to limit the candlesticks to. Defaults to None in  which case fetches the most recent.
+        count (int, optional): The number of trades to fetch. Defaults to 1.
+        daysAgo (int, optional): Can get upto 7 days of data from the most recent trade. Takes in a number between 1 to 7.
+
+    Returns:
+        str: The response containing the trade data as a formatted table.
+    """
+    data = client.get_trades(
+        market=MarketID.from_string(str), to=to, count=count, daysAgo=daysAgo
+    )
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_orderbooks(markets: List[str]) -> str:
+    """
+    Gets trade prices.
+
+    Args:
+        markets (List[str]): A list of markets to fetch month candles for. Each market str is formated like "KRW-BTC".
+    Returns:
+        str: The response containing the orderbook data as a formatted table.
+    """
+    data = client.get_orderbooks(market=MarketID.from_string(str))
+    return _format_helper(data)
+
+
+@mcp.tool()
+async def get_warning_markets() -> dict:
+    """
+    Gets virtual assets that are marked with warnings.
+    """
+    return client.get_warning_markets()
+
+
+@mcp.tool()
+async def get_wallet_status() -> dict:
+    """
+    Retrieves wallet and block statuses.
+    """
+    return client.get_wallet_status()
+
+
+@mcp.tool()
+async def get_api_keys() -> dict:
+    """
+    You can get your Bithumb API keys and the expiry dates.
+    """
+    return client.get_api_keys()
 
 
 if __name__ == "__main__":
